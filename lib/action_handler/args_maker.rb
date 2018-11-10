2c2b708760bdb4a162cc4a4dfbf04c419f813e44
ryym <ryym.64@gmail.com>
2018-11-10 16:09:59 +0900

    feat(args): Allow to specify supplier context

--- lib/action_handler/args_maker.rb
+++ lib/action_handler/args_maker.rb
@@ -4,13 +4,14 @@ module ActionHandler
   class ArgsMaker
     # TODO: Support optional arguments and keyword arguments.
 
-    def make_args(parameters, supplier)
+    def make_args(parameters, supplier, context: nil)
+      supplier_args = [context].compact
       parameters.inject([]) do |values, (_, name)|
         unless supplier.respond_to?(name)
           raise ArgumentError, "parameter #{name} is not defined in #{supplier}"
         end
 
-        values << supplier.send(name)
+        values << supplier.send(name, *supplier_args)
       end
     end
   end
--- spec/action_handler/args_maker_spec.rb
+++ spec/action_handler/args_maker_spec.rb
@@ -13,6 +13,10 @@ describe ActionHandler::ArgsMaker do
         def b
           { bob: true }
         end
+
+        def c
+          :unused
+        end
       end
 
       user_class = Class.new do
@@ -29,6 +33,35 @@ describe ActionHandler::ArgsMaker do
 
       expect(values).to eq([{ bob: true }, [:alice, 1]])
       expect(user.use(*values)).to eq([1, true])
+    end
+
+    context 'with context' do
+      it 'pass context to supplier methods' do
+        supplier_class = Class.new do
+          def hello(name)
+            "hello #{name}"
+          end
+
+          def goodbye(name)
+            "goodbye #{name}"
+          end
+        end
+
+        user_class = Class.new do
+          def use(hello, goodbye)
+            "#{hello}!! #{goodbye}!!"
+          end
+        end
+
+        user = user_class.new
+        params = user.method(:use).parameters
+
+        maker = ActionHandler::ArgsMaker.new
+        values = maker.make_args(params, supplier_class.new, context: 'world')
+
+        expect(values).to eq(['hello world', 'goodbye world'])
+        expect(user.use(*values)).to eq('hello world!! goodbye world!!')
+      end
     end
   end
 end
